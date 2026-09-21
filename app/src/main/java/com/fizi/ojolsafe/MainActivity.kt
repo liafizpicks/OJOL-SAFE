48558f87f14e0c95562b44703c6ca70c1dbef543
liafizpicks <arosmanasopandi@gmail.com>
2026-09-21 10:39:42 +0700

    Create MainActivity class for the app

---
+++ app/src/main/java/com/fizi/ojolsafe/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fizi.ojolsafe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
